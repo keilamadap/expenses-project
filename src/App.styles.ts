ac08319b7efc098d67bf30abfe718e1d61b8657f
Keila <keila.amadap@hotmail.com>
2022-11-10 22:59:56 -0300

    first commit

---
+++ src/App.styles.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color: #0d1117;
+    height: 150px;
+    text-align: center;
+`;
+
+export const Header = styled.div`
+    background-color: #0d1117;
+    height: 150px;
+    text-align: center;
+`;
+
+export const HeaderText = styled.h1`
+    margin: 0;
+    padding: 0;
+    color: #47a6f0;
+    padding-top: 15px;
+   
+`;
+
+export const Body = styled.div`
+    margin: auto;
+    max-width: 960px;
+    margin-bottom: 50px;
+`;
---
+++ src/data/category.ts
@@ -0,0 +1,7 @@
+import { Category } from '../types/Category'
+
+export const catogories: Category = {
+    food: { title: 'Alimentos', color: 'blue', expense: true },
+    rent: { title: 'Aluguel', color: 'brown', expense: true },
+    salary: { title: 'Salario', color: 'green', expense: false },
+}
